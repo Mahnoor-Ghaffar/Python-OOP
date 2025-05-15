39da942fd78170efad4f9c492b31f8684396a713
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-15 12:45:39 +0500

    Auto commit: 2025-05-15 12:45:39.193379

--- practice/main.py
+++ practice/main.py
@@ -27,4 +27,11 @@ print("Perimeter:", c.perimeter())
 # Write a Python program to create a person class. Include attributes like name, country and date of birth. Implement a method to determine the person's age.
 
 class Person:
-    
+    def __init__(self,name,country,date_of_birth):
+        self.name = name
+        self.country = country
+        self.date_of_birth = date_of_birth
+
+    def calculate_age(self):
+        today = date.today()
+        age = today.year - self.date_of_birth.year
