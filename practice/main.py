eeb3059d5178b5a07c1f5d5c329d3dc39bd1ef8b
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-15 12:40:33 +0500

    Auto commit: 2025-05-15 12:40:32.887250

--- practice/main.py
+++ practice/main.py
@@ -3,21 +3,28 @@
 # Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter.
 
 class Circle:
-    def __init__(self,radius):
+    def __init__(self, radius):
         self.radius = radius
 
-# Area=π×r^2
-
-def area(self):
+    # Area = π * r^2
+    def area(self):
         return 3.14159 * self.radius * self.radius
 
-# Perimeter=2×π×r
+    # Perimeter = 2 * π * r
+    def perimeter(self):
+        return 2 * 3.14159 * self.radius
 
-def perimeter(self):
-    return 2 * 3.14159 * self.radius
-
+# Object banao
 c = Circle(5)
 
 print("Radius:", c.radius)
 print("Area:", c.area())
-print(f"Radius: {c.perimeter}")
+print("Perimeter:", c.perimeter())
+
+
+
+# 2. Person Class with Age Calculation
+# Write a Python program to create a person class. Include attributes like name, country and date of birth. Implement a method to determine the person's age.
+
+class Person:
+    
