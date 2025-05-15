e7781b0ebc3adb3980b6ff5e1d0066bbd28226f7
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-15 12:23:44 +0500

    Auto commit: 2025-05-15 12:23:38.691439

---
+++ practice/main.py
@@ -0,0 +1,10 @@
+# 1. Circle Class for Area and Perimeter
+
+# Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter.
+
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+
+def area(self):
+    return 3.14
