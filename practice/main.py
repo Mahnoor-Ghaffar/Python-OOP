ad444b15eb50d09ffb78c7f5c4cd3d2d59bcb25c
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-15 14:28:31 +0500

    Auto commit: 2025-05-15 14:28:27.470255

--- practice/main.py
+++ practice/main.py
@@ -5,24 +5,24 @@ import math
 
 # Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter.
 
-# class Circle:
-#     def __init__(self, radius):
-#         self.radius = radius
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
 
-#     # Area = π * r^2
-#     def area(self):
-#         return 3.14159 * self.radius * self.radius
+    # Area = π * r^2
+    def area(self):
+        return 3.14159 * self.radius * self.radius
 
-#     # Perimeter = 2 * π * r
-#     def perimeter(self):
-#         return 2 * 3.14159 * self.radius
+    # Perimeter = 2 * π * r
+    def perimeter(self):
+        return 2 * 3.14159 * self.radius
 
-# # Object banao
-# c = Circle(5)
+# Object banao
+c = Circle(5)
 
-# print("Radius:", c.radius)
-# print("Area:", c.area())
-# print("Perimeter:", c.perimeter())
+print("Radius:", c.radius)
+print("Area:", c.area())
+print("Perimeter:", c.perimeter())
 
 
 
