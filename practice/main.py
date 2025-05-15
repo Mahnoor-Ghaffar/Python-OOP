21523cb384da9a86b73441fdd8ba6425d506ab29
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-15 14:17:56 +0500

    Auto commit: 2025-05-15 14:17:54.542121

--- practice/main.py
+++ practice/main.py
@@ -102,3 +102,8 @@ class Shape:
 class Circle(Shape):
     def __init__(self,radius):
         self.radius = radius
+
+    def calculate_area(self):
+        return math.pi * self.radius**2
+
+    def calculate_perimeter(self):
