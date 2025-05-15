11c928a3a7ee40e5a0bd5c0945cdb21eb4fb17c8
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-15 12:50:42 +0500

    Auto commit: 2025-05-15 12:50:42.436362

--- practice/main.py
+++ practice/main.py
@@ -35,3 +35,8 @@ class Person:
     def calculate_age(self):
         today = date.today()
         age = today.year - self.date_of_birth.year
+        if today < date(today.year,self.date_of_birth.month,self.date_of_birth.day):
+            age -= 1
+        return age
+
+        
