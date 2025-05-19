3a9e64ed494060a22c876ea32ac9151ad07fb901
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-19 18:04:29 +0500

    Auto commit: 2025-05-19 18:04:28.946486

--- Constructors/main.py
+++ Constructors/main.py
@@ -1,44 +1,10 @@
-#  Constructor in Python
-class Student:
-    def __init__(self,name,age): # Constructor
-        self.name = name
-        self.age = age
-
-    def display(self):
-        print(f"Student Name: {self.name}, Age: {self.age}")
-
-# Object banate waqt constructor automatically call hota hai
-s1 = Student("Mahnoor", 20)
-s1.display()
-
-# -----------Parameterized Constructor
-class Book:
-    def __init__(self,title,author):
-        self.title = title
-        self.author = author
-
-    def show_info(self):
-        print(f"Book Title: {self.title}")
-        print(f"Author: {self.author}")
-
-# Object banate waqt values (parameters) pass ki gayi hain
-book1 = Book("Jannat Ke Pattay", "Nimra Ahmed")
-book1.show_info()
-
-
-# Python’s Own Default Constructor
-class Student:
-    pass
-
-s1 = Student() # Default constructor is called
-print("Object created of class Student")
 
 
 #=================== __del__() in Python
 
 class Person:
     def __init__(self,name):
-        self.name
+        self.name = name
         print(f"{self.name} object created.")
 
     def __del__(self):
