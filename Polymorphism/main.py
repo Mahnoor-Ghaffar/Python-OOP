adf52fa1d8c5a38493e683f5671412a727018723
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-19 15:11:50 +0500

    Auto commit: 2025-05-19 15:11:49.612027

--- Polymorphism/main.py
+++ Polymorphism/main.py
@@ -1,2 +1,11 @@
 # Polymorphisim
-from abc import
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def speak(self) -> None:
+        pass
+
+class Dog(Animal):
+    def speak(self)-> None:
+        print(type(self), ":Woof")
