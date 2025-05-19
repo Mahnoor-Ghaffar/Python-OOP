b789ee95f94b534ca0888f5fc0f166cbcc7f3298
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-19 14:52:08 +0500

    Auto commit: 2025-05-19 14:52:08.432100

--- Inheritance/main.py
+++ Inheritance/main.py
@@ -13,4 +13,11 @@ class Car(Vehicle): # Car inherits from Vehicle
     def __init__(self,color,speed):
         Vehicle().__init__(color,speed)
 
-class Truck(Vehicle):
+class Truck(Vehicle): # Truck inherits from Vehicle
+    def __init__(self,color,speed):
+        Vehicle().__init__(color,speed)
+
+car = Car("red",120)
+truck = Truck("blue",80)
+car.drive()
+truck.drive()
