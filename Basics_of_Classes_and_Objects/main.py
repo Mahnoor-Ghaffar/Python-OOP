8534434b2d3cebe68809f3246eee8f809a7ac87e
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-19 16:58:41 +0500

    Auto commit: 2025-05-19 16:58:41.271372

---
+++ Basics_of_Classes_and_Objects/main.py
@@ -0,0 +1,10 @@
+# Define the Vehicle class
+
+class Vehicle:
+    # Constructor method (called when an object is created)
+    def __init__(self,color,speed):
+        self.color = color  # Attribute
+        self.speed = speed  # speed
+
+    # Method to accelerate the vehicle
+    def accelerate()
