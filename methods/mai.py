114654badfa8460c36b18c1e65a581467a45e959
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-20 01:40:54 +0500

    Auto commit: 2025-05-20 01:40:53.929721

--- methods/mai.py
+++ methods/mai.py
@@ -6,3 +6,9 @@ class MyClass(object):
     def __init__(self):
         self.instance_variable = "Default Value"
         print("Constructor")
+
+    def instance_method(self):
+        print("\n\nInstance Method: " + str(self))
+        print("Calling instance variable: ", self.instance_variable)
+        #print("Calling class variable: ", self.class_variable)
+        #MyClass.class_variable = "Modified Class Variable"
