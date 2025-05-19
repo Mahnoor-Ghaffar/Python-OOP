342e2fe4715af00f999a7256a417c4877bb53bb5
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-19 17:08:52 +0500

    Auto commit: 2025-05-19 17:08:52.332887

--- Basics_of_Classes_and_Objects/main.py
+++ Basics_of_Classes_and_Objects/main.py
@@ -16,4 +16,17 @@ class Vehicle:
         self.speed -= decrement
         print(f"Braking! New speed: {self.speed} km/h")
 
-    # Method to brak
+    # Method to brake to Vehical
+    def display(self):
+        print(f"Vehicle color: {self.color}, Current speed: {self.speed} km/h")
+
+# Create an object (instance) of the Vehicle class
+my_car = Vehicle("Red", 60)
+
+# Access attributes
+print(f"My car is {my_car.color}.")  # Output: My car is Red.
+
+# Call methods
+my_car.accelerate(20)  # Output: Accelerating! New speed: 80 km/h
+my_car.brake(10)       # Output: Braking! New speed: 70 km/h
+my_car.display()       # Output: Vehicle color: Red, Current speed: 70 km/h
