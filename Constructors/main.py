e579b9dec4b29489bbbd3c22fbca859574a035a3
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-19 17:34:08 +0500

    Auto commit: 2025-05-19 17:34:07.944640

--- Constructors/main.py
+++ Constructors/main.py
@@ -10,3 +10,17 @@ class Student:
 # Object banate waqt constructor automatically call hota hai
 s1 = Student("Mahnoor", 20)
 s1.display()
+
+# -----------Parameterized Constructor
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+
+    def show_info(self):
+        print(f"Book Title: {self.title}")
+        print(f"Author: {self.author}")
+
+# Object banate waqt values (parameters) pass ki gayi hain
+book1 = Book("Jannat Ke Pattay", "Nimra Ahmed")
+book1.show_info()
