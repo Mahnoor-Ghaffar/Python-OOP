585c8c5ec52e1aa75f56359e73a84b85b996e8cb
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-20 22:13:52 +0500

    Auto commit: 2025-05-20 22:13:52.621270

--- Uniform_Access/main.py
+++ Uniform_Access/main.py
@@ -10,3 +10,10 @@ class Person:
 p=Person("abc")
 print(p.name)
 print(p.greet())
+
+name = "Mahnoor"
+print(dir(name))
+
+# Ye list object ke sare methods/attributes batayega — append, pop, sort, etc.
+items = [1, 2, 3]
+print(dir(items))
