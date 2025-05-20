519d121b8de631214951f19d0c03fd93b478fc07
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-20 13:07:24 +0500

    Auto commit: 2025-05-20 13:07:23.950879

--- methods/class.py
+++ methods/class.py
@@ -11,4 +11,19 @@ class Student:
     @classmethod
     def Change_School(cls,new_name):
         cls.school_name = new_name
-        
+
+    def display(self):
+        print(f"Student Name: {self.name}, School: {Student.school_name}")
+
+
+s1 = Student("Mahrukh")
+s2= Student("Noor")
+
+print(s1.display())
+print(s2.display())
+
+Student.Change_School("Educators")
+s2.Change_School("abc")
+
+print(s1.display())
+print(s2.display())
