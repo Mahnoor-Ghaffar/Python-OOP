ee053e36f458415645d1f8d7c544658eed8eb7e0
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-20 12:57:18 +0500

    Auto commit: 2025-05-20 12:57:18.648674

--- methods/class.py
+++ methods/class.py
@@ -5,4 +5,10 @@ class Student:
     # Class variable
     school_name = "Saylani Institute"
 
-    def __init__(e)
+    def __init__(self,name):
+        self.name = name
+
+    @classmethod
+    def Change_School(cls,new_name):
+        cls.school_name = new_name
+        
