5c9291fa9de80e4d639061e5ee4774a1044228f6
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-20 17:06:53 +0500

    Auto commit: 2025-05-20 17:06:52.657153

--- Duck_typing/main.py
+++ Duck_typing/main.py
@@ -1,1 +1,17 @@
-D
+#-----------Duck Typing
+
+# if its walk like a duck and talk like a duck its a duck
+
+class Dog:
+    def speak(self):
+        print("Bark!")
+
+class Cat:
+    def speak(self):
+        print("Meow!")
+
+class Human:
+    def speak(self):
+        print("Hello!")
+
+def call_speak
