0722169baa767db062a6c7ca8c31a3a262d04f8f
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-20 22:08:48 +0500

    Auto commit: 2025-05-20 22:08:47.885098

--- Uniform_Access/main.py
+++ Uniform_Access/main.py
@@ -1,5 +1,12 @@
 # access with .
 
 class Person:
-    def __init__(sself,name):
-        
+    def __init__(self,name):
+        self.name = name
+
+    def greet(self):
+        return "Hello " + self.name
+
+p=Person("abc")
+print(p.name)
+print(p.greet())
