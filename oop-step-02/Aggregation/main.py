fbccbf93ceedc77633296fa06a1dbabbcaa840c6
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-21 20:58:47 +0500

    Auto commit: 2025-05-21 20:58:47.508230

--- oop-step-02/Aggregation/main.py
+++ oop-step-02/Aggregation/main.py
@@ -6,3 +6,18 @@ class Car:
     def __init__(self, engine):
         self.engine = engine  # Aggregation (Car has-a Engine)
         print("Car created with Engine")
+
+        def start(self):
+        if self.engine:
+            self.engine.start()
+        else:
+            print("No engine to start")
+
+# Engine object already exists independently
+my_engine = Engine()
+
+# Engine object passed into Car — Aggregation
+my_car = Car(my_engine)
+
+# Start the car
+my_car.start()
